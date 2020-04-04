b7c58b88b98309c97077135ec7bd62034055aa48
Josh Bakos <jzbakos@gmail.com>
2020-04-04 18:15:23 -0400

    created controllers structure & functionality

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,35 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
+let controllers = require("../controllers/index");
+
+/* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Joshs Portfolio' });
+  controllers.displayHome(res);
+});
+
+router.get('/home', function(req, res, next) {
+  controllers.displayHome(res);
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  controllers.displayAbout(res);
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  controllers.displayProjects(res);
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  controllers.displayServices(res);
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  controllers.displayContact(res);
 });
 
 module.exports = router;
